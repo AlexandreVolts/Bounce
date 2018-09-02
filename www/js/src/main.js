e287e6441a96645b6fa20ffad536a519ea27bb68
Alexandre Volts <alexandre.cochet@epitech.eu>
2018-09-02 18:40:16 +0200

    A little detail removed ^^'

--- www/js/src/main.js
+++ www/js/src/main.js
@@ -3,8 +3,8 @@ function main()
 	let canvas = document.getElementsByTagName("canvas")[0];
 	let game;
 
-	canvas.width = 400;//window.innerWidth;
-	canvas.height = 700;//window.innerHeight;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 	game = new Game(canvas);
 
 	game.render();
